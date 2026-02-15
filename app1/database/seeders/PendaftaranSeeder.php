c809e729826cdaaae228f776bef08116ee357a78
seftinaulia <135949506+seftinaulia@users.noreply.github.com>
2026-02-15 17:46:07 +0700

    Add files via upload

---
+++ app1/database/seeders/PendaftaranSeeder.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PendaftaranSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('pendaftaran')->insert([
+            'id_pendaftaran' => 'PENDPSB00001',
+            'user_id' => 2,
+            'nisn' => '92348927348',
+            'nik' => '72619873972094293',
+            'nama_siswa' => 'Seftina',
+            'jenis_kelamin' => 'Perempuan',
+            'pas_foto' => null,
+            'tempat_lahir' => 'Lamongan',
+            'tanggal_lahir' => now(),
+            'agama' => 'Islam',
+
+            //kontak
+            'email'  => 'emailsefti@gmail.com',
+            'hp'  => '238742342340',
+
+            //alamat
+            'alamat'  => 'data pendaftar alamat',
+
+
+            //data pendaftaran
+            'gelombang'  => 1,
+            'tahun_masuk'  => '2021',
+            'pil1'  => 1,
+            'pil2'  => 2,
+
+            //data ortu
+            'nama_ayah'  => 'data pendaftar nama_ayah',
+            'nama_ibu'  => 'data pendaftar nama_ibu',
+            'pendidikan_ayah'  => 'data pendaftar pendidikan_ayah',
+            'pendidikan_ibu'  => 'data pendaftar pendidikan_ibu',
+            'nohp_ayah'  => 'data pendaftar nohp_ayah',
+            'nohp_ibu'  => 'data pendaftar nohp_ibu',
+            'pekerjaan_ayah'  => 'data pendaftar pekerjaan_ayah',
+            'pekerjaan_ibu'  => 'data pendaftar pekerjaan_ibu',
+            'penghasilan_ayah'  => 'data pendaftar penghasilan_ayah',
+            'penghasilan_ibu'  => 'data pendaftar penghasilan_ibu',
+
+            'berkas_ortu'  => 'path_ktp',
+
+
+            //data nilai dan sekolah
+            'sekolah'  => 2,
+            'smt1'  => 99,
+            'smt2'  => 92,
+            'smt3'  => 93,
+            'smt4'  => 94,
+            'smt5'  => 95,
+            'smt6'  => 96,
+            'berkas_siswa'  => 'path_raport',
+            'prestasi'  => 'path_prestasi',
+
+            // TODO: belum jadi
+            'tgl_pendaftaran' => now(),
+            'created_at' => now(),
+            'status_pendaftaran'  => "Selesai",
+        ]);
+
+        DB::table('pembayaran')->insert([
+            'id_pembayaran' => 'PEMPSB00001',
+            'status' => "Dibayar",
+            'verifikasi'  => true,
+            'jatuh_tempo'  => Carbon::now()->addDays(2)->format('Y-m-d'),
+            'tgl_pembayaran'  => Carbon::now()->addDays(1)->format('Y-m-d'),
+            'total_bayar'  => 150000,
+            'bukti_pembayaran'  => 'file/sample-buktipembayaran.jpg',
+            'id_pendaftaran' => 1,
+            'created_at' => Carbon::now(),
+        ]);
+
+        DB::table('pengumuman')->insert([
+            'id_pengumuman' => 'PENGSB00001',
+            'status' => false,
+            'hasil_seleksi'  => 'LULUS',
+            'prodi_penerima' => 1,
+            'nilai_interview' => 100,
+            'nilai_test' => 100,
+            'id_pendaftaran' => 1,
+            'user_id' => 2,
+            'created_at' => Carbon::now(),
+        ]);
+    }
+}
